00655e8d6e68bb3d675f9f4f0a70cd5bb0d5f110
CodingWithJiro <chavezelmar03@gmail.com>
2026-01-11 19:44:41 +0800

    feat(logic): create initial script for shorten link logic

--- js/index.js
+++ js/index.js
@@ -1,2 +1,29 @@
 const input = document.getElementById("shorten-input");
 const button = document.querySelector(".shortener__form-btn");
+
+async function getData(inputUrl) {
+  try {
+    const response = await fetch("http://localhost:3000/shorten", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `url=${encodeURIComponent(inputUrl)}`,
+    });
+
+    const data = await response.json();
+    return data.result_url;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function handleClick() {
+  const data = await getData(input.value);
+  console.log(data);
+}
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  handleClick();
+});
