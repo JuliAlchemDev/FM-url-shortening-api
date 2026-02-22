ec45a32295e732153f2be884e6279923c6a9bd88
Julia Alkhimova <devalchem@gmail.com>
2026-02-22 10:18:21 +0100

    refactor(api): move getData to api.js

---
+++ js/api.js
@@ -0,0 +1,20 @@
+// export async function getData(inputUrl) {
+//   try {
+//     const response = await fetch(
+//       `https://is.gd/create.php?format=json&url=${encodeURIComponent(inputUrl)}`
+//     );
+//     const data = await response.json();
+//     return data.shorturl;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+export async function getData(inputUrl) {
+    const response = await fetch(
+      `https://is.gd/create.php?format=json&url=${encodeURIComponent(inputUrl)}`
+    );
+    const data = await response.json();
+    return data.shorturl;
+
+}
